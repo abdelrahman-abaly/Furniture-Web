0a02f865836926f184667de027faaa077442eeec
norhan <norhanmahmoud447@gmail.com>
2025-01-08 21:50:11 +0200

    add some changes

--- shop.js
+++ shop.js
@@ -136,7 +136,30 @@ xhr.onreadystatechange = function () {
                 parent.appendChild(div);
             }
         });
+        
     }
 };
 
 xhr.send();
+
+
+// 
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
